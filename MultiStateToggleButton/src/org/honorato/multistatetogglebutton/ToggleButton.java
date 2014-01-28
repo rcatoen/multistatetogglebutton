d9321bffe111f176c64990d64fad759a249d9af2
jlhonora <jlhonora@ing.puc.cl>
2014-01-28 16:37:10 -0300

    Initial commit

---
+++ MultiStateToggleButton/src/org/honorato/multistatetogglebutton/ToggleButton.java
@@ -0,0 +1,36 @@
+package org.honorato.multistatetogglebutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public abstract class ToggleButton extends LinearLayout {
+
+	public interface OnValueChangedListener {
+		public void onValueChanged(int value);
+	}
+	
+	OnValueChangedListener listener;
+	Context context;
+	
+	public ToggleButton(Context context) {
+		super(context, null);
+		this.context = context;
+	}
+	
+    public ToggleButton(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        this.context = context;
+    }
+	
+	public void setOnValueChangedListener(OnValueChangedListener l) {
+		this.listener = l;
+	}
+	
+	public void setValue(int value) {
+		if(this.listener != null) {
+			listener.onValueChanged(value);
+		}
+	}
+}
---
+++ MultiStateToggleButtonApp/src/com/example/multistatetogglebuttonapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.multistatetogglebuttonapp;
+
+import org.honorato.multistatetogglebutton.ThreeStateToggleButton;
+import org.honorato.multistatetogglebutton.ToggleButton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	protected static final String TAG = "MainActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		ThreeStateToggleButton button = (ThreeStateToggleButton) this.findViewById(R.id.mstb_main_id);
+		button.setOnValueChangedListener(new ToggleButton.OnValueChangedListener() {
+			
+			@Override
+			public void onValueChanged(int value) {
+				Log.d(TAG, "Value: " + value);
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
